c20cdfcc46817baf546078bf50067f9e56c5301d
DucMinh <116259294+minh12200412@users.noreply.github.com>
2024-09-10 12:54:09 +0700

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,22 +1,19 @@
 // 霸都丶傲天 2019.10.10
 $(function () {
     let dom = document.createElement("span");
-    let delay = 4000;
-    let fadeDuration = 3000;
+    let delay = 2000;
     config.texts.forEach(function (item, index) {
         let p = document.createElement("p");
         p.innerHTML = item;
-        p.style.display = 'none';
         if (config.imgs && config.imgs[item]) {
             let img = document.createElement("img");
             img.src = config.imgs[item];
             img.setAttribute("class", 'text-img');
             p.appendChild(img);
         }
-        dom.appendChild(p);
-        setTimeout(function () {
-            $(p).fadeIn(fadeDuration); // Dùng fadeIn để hiển thị từ từ
-        }, delay * (index + 1));
+       setTimeout(function() {
+            dom.appendChild(p);
+        }, index * delay);
     });
     $("#texts-container").append(dom.innerHTML);
 });
